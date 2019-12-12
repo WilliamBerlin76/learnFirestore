682a3569f368dd9457f4f8205dff03555c5e5602
WilliamBerlin76 <william.berlin76@gmail.com>
2019-12-12 17:53:43 -0600

    getting data from the db and logging it to the console

--- index.js
+++ index.js
@@ -1,3 +1,45 @@
 const server = require("./api/server");
 
+const admin = require('firebase-admin');
+
+let serviceAccount = require('./key/learningfirestore-261822-4f3dbc0c9255.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+let db = admin.firestore();
+
+/////////////////////////////////////SETTING DATA/////////////////
+
+let docRef = db.collection('users').doc('alovelace');
+
+let setAda = docRef.set({
+  first: 'Ada',
+  last: 'Lovelace',
+  born: 1815
+});
+
+
+
+let aTuringRef = db.collection('users').doc('aturing');
+
+let setAlan = aTuringRef.set({
+  'first': 'Alan',
+  'middle': 'Mathison',
+  'last': 'Turing',
+  'born': 1912
+});
+
+///////////////////GET DATA//////////////////////
+db.collection('users').get()
+  .then((snapshot) => {
+    snapshot.forEach((doc) => {
+      console.log(doc.id, '=>', doc.data());
+    });
+  })
+  .catch((err) => {
+    console.log('Error getting documents', err);
+  });
+
 server.listen(5000, () => console.log(`\n=== server listening on port 5000 === \n`))
