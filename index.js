8f5bae61782ff775820d4730ca65d3ee27a26b25
WilliamBerlin76 <william.berlin76@gmail.com>
2019-12-13 18:53:54 -0600

    added update and delete CRUD funcitonality for the users collection

--- index.js
+++ index.js
@@ -89,17 +89,51 @@ server.get('/users', (req, res) => {
     })
 });
 
+///////////////////////////// POST TO USERS COLLECTION ////////////////////////////////////
 server.post('/users', (req, res) => {
   const docu = req.body.docname;
   const details = req.body.details;
   db.collection('users').doc(docu).set(details)
     .then(user => {
       const userObj = {docName: docu, details}
-      res.status(200).json(userObj)
+      res.status(201).json(userObj)
     })
     .catch(err => {
       console.log(err)
       res.status(500).json({ error: 'there was an error adding the user to the server'})
     })
 })
+
+
+//////////////////// PUT REQUEST TO USERS COLLECTION BY DOCUMENT ////////////////////
+server.put('/users/:doc', (req, res) => {
+  const docu = req.body.docname;
+  const details = req.body.details;
+
+  db.collection('users').doc(docu).update(details)
+    .then(user => {
+      const userObj = {docName: docu, details}
+      res.status(200).json(userObj)
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({ error: 'there was an error updating the user on the server'})
+    })
+})
+
+//////////////////////////// DELETE A USER FROM THE COLLECTION
+
+server.delete('/users/:docName', (req, res) => {
+  const docName = req.params.docName
+  db.collection('users').doc(docName).delete()
+    .then(user => {
+      res.status(200).json({message: 'the selected doc was deleted'})
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({ error: 'there was an error removing the doc from the db' })
+    })
+});
+
+
 server.listen(5000, () => console.log(`\n=== server listening on port 5000 === \n`))
